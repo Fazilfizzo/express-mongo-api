3aac4da336541ac6b1836adb3460053f7b3042d9
Fazilfizzo developer <fadhilimajala97@gmail.com>
2025-04-25 17:31:50 +0000

    fix: API now working fine

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import studentRoutes from './routes/students.js';
 import subjectRoutes from './routes/subjects.js';
 
@@ -7,15 +8,21 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
-// MongoDB connection (No .env used)
+
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
-app.use('/students', studentRoutes);
-app.use('/subjects', subjectRoutes);
+  app.use(express.json());
+
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+
+app.use('/api/students', studentRoutes);
+app.use('/api/subjects', subjectRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
